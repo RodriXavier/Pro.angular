5bd215080314301e4425830815f1c982c157d6dc
RodriXavier <rodrigokeelover@gmail.com>
2022-10-23 21:27:42 -0300

    incio

---
+++ src/app/components/dashboard/dashboard.module.ts
@@ -0,0 +1,29 @@
+import { SharedModule } from './../shared/shared.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardComponent } from './dashboard.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ProveedoresComponent } from './proveedores/proveedores.component';
+import { ProductosComponent } from './productos/productos.component';
+import { VentaComponent } from './venta/venta.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    InicioComponent,
+    ProveedoresComponent,
+    ProductosComponent,
+    VentaComponent,
+    NavbarComponent
+  ],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+    SharedModule
+  ]
+})
+export class DashboardModule { }
